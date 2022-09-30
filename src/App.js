f781b19adf7cd5cf760b348175eb535485e66f48
ChristianL01 <locke.christian1@gmail.com>
2022-09-30 15:44:01 -0500

    Add and import homepage component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import Nav from "./nav/Nav";
 import './CSS/Style.css'
+import Homepage from "./Homepage/Homepage";
 
 function App() {
   return (
     <div className="App">
     <h1 className="h1">Infinite Cocktail</h1>
     <nav><Nav /></nav>
+    <body>
+      <Homepage />
+    </body>
     </div>
   );
 }
