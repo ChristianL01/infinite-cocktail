47482befe35d91fc6cd60449ba0c51c16928863a
ChristianL01 <locke.christian1@gmail.com>
2022-09-30 16:13:40 -0500

    Retrieve data from api

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import Nav from "./nav/Nav";
 import './CSS/Style.css'
 import Homepage from "./Homepage/Homepage";
-
+import { useState } from 'react'
+const baseUrl = 'https://www.thecocktaildb.com/api/json/v1/1/';
 function App() {
+const [url , setUrl] = useState(baseUrl)
   return (
     <div className="App">
     <h1 className="h1">Infinite Cocktail</h1>
     <nav><Nav /></nav>
-    <body>
-      <Homepage />
-    </body>
+    <main>
+      <Homepage url={url} />
+    </main>
     </div>
   );
 }
--- src/Homepage/Homepage.js
+++ src/Homepage/Homepage.js
@@ -1,11 +1,22 @@
 import React from 'react';
 
-function Homepage(props) {
-    return (
-        <div>
-            Hello
-        </div>
-    );
+function Homepage({ url }) {
+	const randUrl = { url } + '/random.php';
+	function handleClick() {
+		fetch('https://www.thecocktaildb.com/api/json/v1/1/random.php')
+			.then((res) => {
+				return res.json();
+			})
+			.then((res) => {
+				console.log('success!', res);
+			})
+			.catch((err) => console.log('something went wrong...', err));
+	}
+	return (
+		<div>
+			<button onClick={handleClick}>Find Random Cocktail</button>
+		</div>
+	);
 }
 
 export default Homepage;
