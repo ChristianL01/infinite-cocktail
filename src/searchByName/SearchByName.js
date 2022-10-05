59ea89911c79cb30fba4bd3f525bfa8ca1112895
ChristianL01 <locke.christian1@gmail.com>
2022-10-05 11:57:47 -0500

    Recieve correct data from cocktail name input

--- src/searchByName/SearchByName.js
+++ src/searchByName/SearchByName.js
@@ -1,8 +1,18 @@
 import React from 'react';
+import { useState } from 'react';
 
 function SearchByName(props) {
-    function handleClick() {
-		fetch('https://www.thecocktaildb.com/api/json/v1/1/search.php?s=')
+	const [url, setUrl] = useState(
+		'https://www.thecocktaildb.com/api/json/v1/1/search.php?s='
+	);
+    console.log(url)
+	const [name, setName] = useState('');
+	function handleChange(e) {
+		setName(e.target.value);
+		console.log(e.target.value);
+	}
+	function handleClick() {
+		fetch(url + name)
 			.then((res) => {
 				return res.json();
 			})
@@ -11,13 +21,19 @@ function SearchByName(props) {
 			})
 			.catch((err) => console.log('something went wrong...', err));
 	}
-    return (
-        <div>
-            <label htmlFor="">Margarita Name</label>
-            <input type="text" placeholder='Margarita Name' />
-            <button type='submit'>Look Up Cocktail By Name</button>
-        </div>
-    );
+	return (
+		<div>
+			<label htmlFor=''>Cocktail Name</label>
+			<input
+				className='name-input'
+				onChange={handleChange}
+				value={name}
+				type='text'
+				placeholder='Cocktail Name'
+			/>
+			<button onClick={handleClick}>Look Up Cocktail By Name</button>
+		</div>
+	);
 }
 
 export default SearchByName;
